d88e0d795edb06a0f53fd433f40e704a01e57742
ttben <bb102748@etu.unice.fr>
2014-03-26 10:35:49 +0100

    Ajout des classes Main et Controller

---
+++ src/devint/Main.java
@@ -0,0 +1,12 @@
+package devint;
+
+import devint.controller.Controller;
+
+/**
+ * Created by user on 26/03/14.
+ */
+public class Main {
+    public static void main(String[] argc) {
+        new Controller();
+    }
+}
